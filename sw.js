84a567601ae068e960775484520cd78ddef1f038
yangzijie <yangzijie@laiye.com>
2020-08-01 11:16:19 +0800

    Updates

--- sw.js
+++ sw.js
@@ -26,23 +26,20 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8e28a6de5b8bef13cdcf.js"
+    "url": "webpack-runtime-9aa22b75f85c3d36a40b.js"
   },
   {
     "url": "framework-376edee25eb5f5cd8260.js"
   },
   {
-    "url": "app-cb2e27b229c4519ec06e.js"
+    "url": "app-2d44c2a41a120d2647f2.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-d731f33f2b184998b449.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "01565748db85243989247c5b984e513b"
-  },
-  {
-    "url": "polyfill-118cb73b15f8ba765669.js"
+    "revision": "cbf455fe138af1ae557c48586691fec4"
   },
   {
     "url": "manifest.json",
@@ -69,12 +66,12 @@ const { NavigationRoute } = workbox.routing
 
 const navigationRoute = new NavigationRoute(async ({ event }) => {
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^`), ``)
+  pathname = pathname.replace(new RegExp(`^https://cdn.jsdelivr.net/gh/byteyang/blog@gh-pages`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-cb2e27b229c4519ec06e.js`))) {
+  if (!resources || !(await caches.match(`https://cdn.jsdelivr.net/gh/byteyang/blog@gh-pages/app-2d44c2a41a120d2647f2.js`))) {
     return await fetch(event.request)
   }
 
@@ -87,7 +84,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `https://cdn.jsdelivr.net/gh/byteyang/blog@gh-pages/offline-plugin-app-shell-fallback/index.html`
   return await caches.match(offlineShell)
 })
 
